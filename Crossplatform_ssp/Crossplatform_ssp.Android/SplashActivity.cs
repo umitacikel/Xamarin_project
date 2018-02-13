792f66cababc0a9449615c051bee0f75a0d16e43
Yunas09 <Yunas3000@hotmail.com>
2018-02-13 15:06:38 +0100

    Prøv nu

---
+++ Crossplatform_ssp/Crossplatform_ssp.Android/SplashActivity.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Threading;
+
+namespace Crossplatform_ssp.Droid
+{
+
+    [Activity(Label = "SSP", MainLauncher = true, NoHistory = true, Theme = "@style/Theme.SplashActivity")]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Wait for 2 seconds
+            Thread.Sleep(2000);
+
+            //Moving to next activity
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
